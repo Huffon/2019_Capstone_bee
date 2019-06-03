0e241caee1c81599d7fc26fe345e4ee7799e344c
huffonism@gmail.com <huffonism@gmail.com>
2019-06-04 08:06:57 +0900

    add crawler

---
+++ WEB/BEE_WEB/korean_dictionary_crawler.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+word = input()
+
+url = "https://stdict.korean.go.kr/search/searchResult.do?&searchKeyword=" + word
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+result = ""
+try:
+    result += soup.find('ul', {'class': 'result'}).find('dt').find( 'font', {'class': 'dataLine'}).get_text()
+    result = result[1:]
+
+except:
+    result = "사전에 등재되어 있지 않습니다."
+print(result)
