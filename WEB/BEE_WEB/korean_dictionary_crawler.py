282a33cb9a3037697457f9f0b56cd14f74919c40
unknown <2seoyeoni@naver.com>
2019-06-10 17:59:05 +0900

    modify WEB

--- WEB/BEE_WEB/korean_dictionary_crawler.py
+++ WEB/BEE_WEB/korean_dictionary_crawler.py
@@ -8,12 +8,13 @@ url = "https://stdict.korean.go.kr/search/searchResult.do?&searchKeyword=" + wor
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-
 result = ""
+
 try:
     result += soup.find('ul', {'class': 'result'}).find('dt').find( 'font', {'class': 'dataLine'}).get_text()
     result = result[1:]
 
 except:
     result = "사전에 등재되어 있지 않습니다."
+
 print(result)
